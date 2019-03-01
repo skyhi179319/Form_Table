84f6e354823f0ddb5891378d60ce248ecd32bd5e
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2019-03-01 16:22:51 -0700

    Update Form.php

--- Form.php
+++ Form.php
@@ -6,5 +6,11 @@
 <input type="name" name="name3">
 <input type="name" name="name4">
 <input type="name" name="name5">
+<br>
+<input name="number1" type="number">
+<input name="number2" type="number">
+<input name="number3" type="number">
+<input name="number4" type="number">
+<input name="number5" type="number">
 </form>
 </body>
