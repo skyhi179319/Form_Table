4b6597d42d19dc92c35ce68166a080be1c909569
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2019-03-01 16:17:31 -0700

    Create Form.php

---
+++ Form.php
@@ -0,0 +1,10 @@
+<!doctype>
+<body>
+<form>
+<input type="name" name="name1">
+<input type="name" name="name2">
+<input type="name" name="name3">
+<input type="name" name="name4">
+<input type="name" name="name5">
+</form>
+</body>
